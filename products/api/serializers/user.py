a0260ac63298c9783df41d681731a1c2e65d06af
Jesus <jvilas1993@gmail.com>
2016-10-05 11:52:38 -0500

    Refactore user validations
    Refactor user and user profile validations
    Add email field to user

--- products/api/serializers/user.py
+++ products/api/serializers/user.py
@@ -8,63 +8,67 @@ from rest_framework import serializers
 from api.models.user import UserProfile
 
 
+def create_user(user_data, profile=None):
+    print(user_data)
+    user_data['username'] = user_data['email']
+    user_data.pop('password_confirmation')
+    if profile is not None:
+        user = User.objects.create(profile=profile, **user_data)
+    else:
+        user = User.objects.create(**user_data)
+    user.set_password(user_data['password'])
+    user.save()
+    return user
+
+
 class UserSerializer(serializers.ModelSerializer):
     """User serializer."""
-    password = serializers.CharField(style={'input_type': 'password'})
-    password_confirmation = serializers.CharField(
+    password = serializers.CharField(
+        style={'input_type': 'password'},
         max_length=User._meta.get_field('password').max_length,
+        min_length=6)
+    password_confirmation = serializers.CharField(
         style={'input_type': 'password'})
+    email = serializers.EmailField
 
     class Meta:
         model = User
-        fields = ('pk', 'first_name', 'last_name', 'password',
+        fields = ('pk', 'first_name', 'last_name', 'email', 'password',
                   'password_confirmation',)
 
-    def validate(self, attrs):
-        password_length = len(attrs['password'])
-        if password_length < 6:
-            raise serializers.ValidationError({
-                'password': 'Password length must be greater than or '
-                            'equal to 6.'})
+    def validate_email(self, attrs):
+        email = User.objects.filter(email=attrs)
+        if email:
+            raise serializers.ValidationError('Email address already exists.')
         return attrs
 
-    def create(self, validated_data):
-        validated_data.pop('password_confirmation')
-        user = User.objects.create(**validated_data)
-        return user
-
-    def to_representation(self, instance):
-        return {'pk': instance.pk, 'first_name': instance.first_name,
-                'last_name': instance.last_name}
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    """User profile serializer."""
-    user = UserSerializer(many=False)
-
-    class Meta:
-        model = UserProfile
-        fields = ('user', 'phone', 'role',)
-
     def validate(self, attrs):
-        phone_length = len(attrs['phone'])
-        if phone_length < 8 or phone_length > 10:
-            raise serializers.ValidationError({
-                'phone': 'Phone length must be greater than or equal '
-                         'to 8 and less than or equal to 10.'})
-        password = attrs['user']['password']
-        password_confirmation = attrs['user']['password_confirmation']
+        password = attrs['password']
+        password_confirmation = attrs['password_confirmation']
         if password != password_confirmation:
             raise serializers.ValidationError(
                 {'password': 'Password confirmation must match Password'})
         return attrs
 
     def create(self, validated_data):
+        return create_user(validated_data)
+
+    def to_representation(self, instance):
+        return {'pk': instance.pk, 'first_name': instance.first_name,
+                'last_name': instance.last_name, 'email': instance.email}
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """User profile serializer."""
+    user = UserSerializer(many=False)
+    phone = serializers.CharField(min_length=8, max_length=10)
+
+    class Meta:
+        model = UserProfile
+        fields = ('user', 'phone', 'role',)
+
+    def create(self, validated_data):
         user_data = validated_data.pop('user')
-        user_data['username'] = User.objects.count() + 1
-        user_data.pop('password_confirmation')
         profile = UserProfile.objects.create(**validated_data)
-        user = User.objects.create(profile=profile, **user_data)
-        user.set_password(user_data['password'])
-        user.save()
+        create_user(user_data, profile=profile)
         return profile
