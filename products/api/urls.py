c2d9df8617746932cad5b33e15d34e1e400b1183
Jesus <jvilas1993@gmail.com>
2016-10-07 11:02:21 -0500

    Add company
    Add company model and tests

--- products/api/urls.py
+++ products/api/urls.py
@@ -7,6 +7,7 @@ from rest_framework import routers
 
 from api.views import (IndustryViewSet, CountryViewSet, StateViewSet,
                        CategoryViewSet, UserProfileViewSet)
+from api.views.company import CompanyViewSet
 
 router = routers.DefaultRouter()
 router.register(r'industries', IndustryViewSet)
@@ -14,6 +15,7 @@ router.register(r'countries', CountryViewSet)
 router.register(r'states', StateViewSet)
 router.register(r'categories', CategoryViewSet)
 router.register(r'users', UserProfileViewSet)
+router.register(r'companies', CompanyViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
