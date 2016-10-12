74f5e6e0b2c7cc515ad3993f1e7bb884ac876941
Jesus <jvilas1993@gmail.com>
2016-10-12 17:19:14 -0500

    Refactor product serializers

--- products/api/urls.py
+++ products/api/urls.py
@@ -7,20 +7,14 @@ from rest_framework import routers
 
 from api.views import (IndustryViewSet, CountryViewSet, StateViewSet,
                        CategoryViewSet)
-from api.views.product import (ProductViewSet, KeywordViewSet,
-                               AttributeViewSet, ProductAttributeViewSet,
-                               ProductLogisticViewSet)
+from api.views.product import ProductViewSet
 
 router = routers.DefaultRouter()
 router.register(r'industries', IndustryViewSet)
 router.register(r'countries', CountryViewSet)
 router.register(r'states', StateViewSet)
 router.register(r'categories', CategoryViewSet)
-router.register(r'keywords', KeywordViewSet)
-router.register(r'attributes', AttributeViewSet)
-router.register(r'product_attributes', ProductAttributeViewSet)
 router.register(r'products', ProductViewSet)
-router.register(r'product_logistics', ProductLogisticViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
